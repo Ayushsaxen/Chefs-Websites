a290e970065956744a246fdfd5ca6d81b3f9acd7
Ayush <107456381+Ayushsaxen@users.noreply.github.com>
2024-01-06 18:48:21 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+let menu = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
+window.onscroll = () => {
+    navbar.classList.remove('active');
+    menu.classList.remove('bx-x');
+    
+
+}
